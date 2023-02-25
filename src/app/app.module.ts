f54b1c256d6fced6a079838508dd06abb080527b
Riccardo Monaco <gandhi.rimo@gmail.com>
2023-02-25 13:40:16 +0100

    added basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './view/wrapper/menu/menu.component';
+import { PlayerComponent } from './view/wrapper/player/player.component';
+import { ExploreComponent } from './view/wrapper/explore/explore.component';
+import { SearchbarComponent } from './view/wrapper/searchbar/searchbar.component';
+import { WrapperComponent } from './view/wrapper/wrapper.component';
 
-@NgModule({
+NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    PlayerComponent,
+    ExploreComponent,
+    SearchbarComponent,
+    WrapperComponent
   ],
   imports: [
     BrowserModule
